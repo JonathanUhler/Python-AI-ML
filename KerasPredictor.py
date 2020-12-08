f4e5968c9a43c510e1576122920fd2da372c7ee8
Jonathan Uhler <jonathan@uhler.com>
2020-12-08 13:47:59 -0800

    combines the visualizer into KerasPredictor.py as a global boolean at the top of the file, other minor fixes

--- KerasPredictor.py
+++ KerasPredictor.py
@@ -11,6 +11,7 @@ from numpy import loadtxt # NumPy is a number/data processor
 from keras.optimizers import SGD
 from keras.models import Sequential # Keras is the AI/ML API. A Keras model is the entire network
 from keras.layers import Dense # A layer is each set of nodes that make up the network
+import matplotlib.pyplot as plt # MatPlotLib is the library used to visualize the data
 
 
 # =================================================================================================
@@ -18,12 +19,12 @@ from keras.layers import Dense # A layer is each set of nodes that make up the n
 # 
 # Change here for easy access:
 #
-# Load in the data file using NumPy
-dataCSV = 'banknote-authentication.csv'
-dataSeperator = ','
-# The number of input dimensions (the width of the input layer)
-# inputDims = 4 # NOTE: this number should be 1 less than the total number of data points per row
-outputDims = 10
+dataCSV = 'banknote-authentication.csv' # --> The input with with data for training and testing
+dataSeperator = ',' # --> The character that seperates data elements in the input file
+outputDims = 10 # --> The width of the output layer
+visualizer = False # --> The toggle for whether or not a graph is displayed when the NN finishes
+#
+print("dataCSV: " + str(dataCSV) + "\ndataSeperator: " + str(dataSeperator) + "\noutputDims: " + str(outputDims) + "\nvisualizer: " + str(visualizer) + "\n")
 #
 # end: GLOBAL VARIABLES
 
@@ -67,7 +68,10 @@ model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metr
 
 
 # Fit the model to the dataset (run the model for training)
-model.fit(X, Y, epochs = 300, batch_size = 10)
+if (visualizer):
+    history = model.fit(X, Y, validation_split=0.33, epochs=150, batch_size=10, verbose=0)
+else:
+    model.fit(X, Y, epochs = 300, batch_size = 10)
 
 
 # Let the model evaluate itself
@@ -158,3 +162,51 @@ for i in range(len(prediction)):
     print("Test case: " + str(i + 1))
     # Print the best prediction, the useful (or rounded) prediction, and the expected result
     print("Prediction (Actual): " + str(float(bestPred)) + " | Prediction (Useful): " + str(round(float(bestPred), 0)) + " | Expected: " + str(answer) + "\n")
+
+
+# =================================================================================================
+# def showChart
+#
+# A function to load a chart that visualizes the data from the network
+#
+# Arguments--
+#
+# plot1:        the metric from history to plot
+#
+# plot2:        the second metric from history to plot
+#
+# title:        the title of the graph
+#
+# ylabel:       the title of the y-axis
+#
+# xlabel:       the title of the x-axis
+#
+# legend1:      the name of the first plotted datapoint
+#
+# legend2:      the name of the second plotted datapoint
+#
+# legendLoc:    the location of the legend on the screen
+#
+# Arguments--
+#
+# None
+#
+def showChart(plot1, plot2, title, ylabel, xlabel, legend1, legend2, legendLoc):
+    
+    # Assemble the chart
+    plt.plot(history.history[plot1])
+    plt.plot(history.history[plot2])
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.legend([legend1, legend2], loc = legendLoc)
+
+    # Show the chart
+    plt.show()
+# end: def showChart
+
+
+# Call the show chart function
+if (visualizer):
+    showChart('accuracy', 'val_accuracy', 'model accuracy', 'accuracy', 'epoch', 'train', 'test', 'upper left')
+    showChart('loss', 'val_loss', 'model loss', 'loss', 'epoch', 'train', 'test', 'upper left')
